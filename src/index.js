c31716e45fa135c127eda75e083c7a47b4ec4403
carino <carino@miton-no-MacbookPro.local>
2022-02-24 00:39:40 +0900

    コメントアウト修正

--- src/index.js
+++ src/index.js
@@ -6,12 +6,11 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App /> //render()関数の第二引数で参照したDOMに対して<App />(= Appコンポーネント)を紐付ける
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitalsreportWebVitals();
